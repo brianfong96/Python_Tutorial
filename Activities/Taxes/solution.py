e309cec1692ce6251686585cdf255438de3ff1c7
brianfong96 <brianwkfong@gmail.com>
2022-08-20 15:01:35 -0700

    Added all of taxes activity

--- Activities/Taxes/solution.py
+++ Activities/Taxes/solution.py
@@ -2,7 +2,27 @@
 # https://www.canada.ca/en/revenue-agency/services/tax/individuals/frequently-asked-questions-individuals/canadian-income-tax-rates-individuals-current-previous-years.html
 # Examples of the output can be seen in examples.txt
 
+def calculate_tax(income, tax_bracket, tax_rate):
+    total_tax = 0
+    for i in range(len(tax_bracket)):
+        taxable_amount = income
+        if income >= tax_bracket[i]:
+            taxable_amount = min(tax_bracket[i], taxable_amount)
+        if i > 0:
+            taxable_amount -= tax_bracket[i-1]
+        if taxable_amount < 0:
+            return total_tax
+        tax = taxable_amount * tax_rate[i]
+        print(f"You will need to pay ${tax:.2f}({tax_rate[i]*100:.2f}%) on the {'first' if i == 0 else 'next'} ${taxable_amount:.2f}")
+        total_tax += tax
+    return total_tax
+
 if __name__ == "__main__":
+    canada_federal_tax_bracket = [50197, 100392, 155625, 221708, float("inf")]
+    canada_federal_tax_rate = [0.15, 0.205, 0.26, 0.29, 0.33]
+
+    provincial_tax_bracket = {"bc" : [43070, 86141, 98901, 120094, 162832, 227091, float("inf")]}
+    provincial_tax_rate = {"bc" : [0.0506, 0.077, 0.105, 0.1229, 0.147, 0.168, 0.205]}
     valid_input = False
     while not valid_input:
         try:
@@ -13,17 +33,9 @@ if __name__ == "__main__":
         
 
     print("For federal taxes:")
-    # TODO
-    print(f"You will need to pay ${0:.2f}({0:.2f}%) on the {'first/next'} ${0:.2f}")
-    canada_federal_tax = 0
-    # /TODO
-
+    canada_federal_tax = calculate_tax(income, canada_federal_tax_bracket, canada_federal_tax_rate)
     print("For provincial taxes:")
-    # TODO
-    print(f"You will need to pay ${0:.2f}({0:.2f}%) on the {'first/next'} ${0:.2f}")
-    bc_tax = 0
-    # /TODO
-
+    bc_tax = calculate_tax(income, provincial_tax_bracket["bc"], provincial_tax_rate["bc"])
     total_taxes = canada_federal_tax + bc_tax
     income_after_tax = income - total_taxes
 
--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # Python_Tutorial
-Tutorial for Setting and Coding with Python
+Tutorial for Setting up and Coding with Python
 
 # Getting Started
 
 ## Links
 - [VS Code](https://code.visualstudio.com/)
 - [Python](https://www.python.org/downloads/)
+
+## Activities
+Under Activities will be a few examples that you can try coding in Python.
+There will be an examples.txt to show what what some of the script should look like while you run them.
+There is also a solution.py in each activity to show what a solution can look like.
